31832dcf21a6dd77b1b68bb14b05a058f4f7ca2e
Rajat-Karahe <29karaherajat@gmail.com>
2020-04-17 19:40:15 +0530

    Create TrieNodeClass.cpp

---
+++ TrieNodeClass.cpp
@@ -0,0 +1,12 @@
+TrieNode {
+    public:
+    char data;
+    TrieNode** children;
+    bool isTerminal;
+    
+    TrieNode(char data){
+        this->data = data;
+        children = new TrieNode*[26];
+        isTerminal = false;
+    }
+};
